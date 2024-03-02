e8272963b3c0bf296199b642eaa5c292a05dc88d
Jeahoon Jung <jhoon5245@gmail.com>
2024-03-02 13:24:03 +1300

    Updated ListLayoutWithTags to match with rest pages

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -77,13 +77,13 @@ export default function ListLayoutWithTags({
 
   return (
     <>
-      <div>
-        <div className="pb-6 pt-6">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 sm:divide-none">
+        <div className="pb-8 pt-6">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
         </div>
-        <div className="flex sm:space-x-24">
+        <div className="flex sm:space-x-16">
           <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
             <div className="px-6 py-4">
               {pathname.startsWith('/blog') ? (
