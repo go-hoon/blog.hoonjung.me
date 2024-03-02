9158120dc81883c6287173b9816ca400134e85d9
Jeahoon Jung <jhoon5245@gmail.com>
2024-03-02 17:20:21 +1300

    [CICD] Updated next.config.js to export static files
    Deleted image optimization config - it is not compatible with `{ output: 'export' }` because the optimization happens on-demand (not build time).
    https://nextjs.org/docs/messages/export-image-api
    
    Deleted headers config because you can't set headers on response with static output.
    https://nextjs.org/docs/messages/export-no-custom-routes

--- next.config.js
+++ next.config.js
@@ -60,26 +60,14 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
+    output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
+      unoptimized: true,
     },
     webpack: (config, options) => {
       config.module.rules.push({
